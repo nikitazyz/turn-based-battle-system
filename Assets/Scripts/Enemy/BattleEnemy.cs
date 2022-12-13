c3f01d4c3d1c12a535e94b6b55685119ae6387b2
nikitazyz <nikita.zyz16@gmail.com>
2022-12-13 17:37:55 +0300

    Added enemies classes
    Added Enemy, EnemyList and BattleEnemy classes

---
+++ Assets/Scripts/Enemy/BattleEnemy.cs
@@ -0,0 +1,19 @@
+using HealthSystem;
+
+namespace Enemy
+{
+    public class BattleEnemy
+    {
+        public Enemy Enemy { get; }
+
+        public Health Health { get; }
+
+        public BattleEnemy(Enemy enemy, int maxHealth, int healthValue)
+        {
+            Enemy = enemy;
+            Health = new Health(maxHealth, healthValue);
+        }
+
+        public BattleEnemy(Enemy enemy, int healthValue) : this(enemy, healthValue, healthValue) {}
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyList.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Enemy
+{
+    public class EnemyList : IReadOnlyList<BattleEnemy>
+    {
+        private readonly IList<BattleEnemy> _battleEnemies;
+
+        public EnemyList(params BattleEnemy[] battleEnemies)
+        {
+            _battleEnemies = battleEnemies;
+        }
+        
+        public IEnumerator<BattleEnemy> GetEnumerator()
+        {
+            return _battleEnemies.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => _battleEnemies.Count;
+
+        public BattleEnemy this[int index] => _battleEnemies[index];
+    }
+}
