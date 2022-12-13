5fcd18ae0cbf2919c91bca7e7f68e8dc9eff2f25
nikitazyz <nikita.zyz16@gmail.com>
2022-12-13 17:47:50 +0300

    Added Event Bus

---
+++ Assets/Scripts/EventBusSystem/EventBus.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace EventBusSystem
+{
+    public abstract class EventBus
+    {
+        private bool _enabled;
+        public event Action<bool> EnableStateChanged;
+
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                _enabled = value;
+                EnableStateChanged?.Invoke(value);
+            }
+        }
+
+        public void Invoke()
+        {
+            if (Enabled)
+            {
+                OnInvoke();
+            }
+        }
+        protected abstract void OnInvoke();
+
+        protected virtual void OnActiveChanged() { }
+        
+        public EventBus()
+        {
+            Enabled = true;
+        }
+        
+        public EventBus(bool enabled)
+        {
+            Enabled = enabled;
+        }
+    }
+}
