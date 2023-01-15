0a8c8b5628c0ba125b9a6b064388542d9724f97a
nikitazyz <nikita.zyz16@gmail.com>
2023-01-15 22:11:34 +0300

    Changed editor scripts namespaces
    - AbilityEffectListPropertyDrawer.cs and ScenePathAttributeDrawer.cs now in global namespace

--- Assets/Editor/ScenePathAttributeDrawer.cs
+++ Assets/Editor/ScenePathAttributeDrawer.cs
@@ -2,33 +2,32 @@ using SceneLoadSystem;
 using UnityEngine;
 using UnityEditor;
 
-namespace Editor
+
+[CustomPropertyDrawer(typeof(ScenePathAttribute))]
+public class ScenePathAttributeDrawer : PropertyDrawer
 {
-    [CustomPropertyDrawer(typeof(ScenePathAttribute))]
-    public class ScenePathAttributeDrawer : PropertyDrawer
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        if (property.type != "string")
         {
-            if (property.type != "string")
-            {
-                return;
-            }
-
-            var style = new GUIStyle(EditorStyles.textField);
-            
-            if (!property.stringValue.StartsWith("Assets") || AssetDatabase.LoadAssetAtPath<SceneAsset>(property.stringValue) == null)
-            {
-                style.normal.textColor = Color.red;
-                style.hover.textColor = Color.red;
-            }
-
-            position.width -= 30;
-            property.stringValue = EditorGUI.TextField(position, label, property.stringValue, style);
-
-            position.x += position.width + 2;
-            position.width = 28;
-
-            GUI.Button(position, "");
+            return;
         }
+
+        var style = new GUIStyle(EditorStyles.textField);
+
+        if (!property.stringValue.StartsWith("Assets") ||
+            AssetDatabase.LoadAssetAtPath<SceneAsset>(property.stringValue) == null)
+        {
+            style.normal.textColor = Color.red;
+            style.hover.textColor = Color.red;
+        }
+
+        position.width -= 30;
+        property.stringValue = EditorGUI.TextField(position, label, property.stringValue, style);
+
+        position.x += position.width + 2;
+        position.width = 28;
+
+        GUI.Button(position, "");
     }
 }
