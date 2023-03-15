ea8212bc5284d0a53ab14177837ecb9a01a91fce
nikitazyz <nikita.zyz16@gmail.com>
2023-03-15 21:33:28 +0300

    Added maxHealth for enemy

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -8,11 +8,14 @@ namespace Enemies
     public class Enemy : ScriptableObject
     {
         [SerializeField] private Dice _battleDice;
+        [SerializeField] private int _maxHealth;
         
         [SerializeField] private RuntimeAnimatorController _animatorController;
 
         public RuntimeAnimatorController AnimatorController => _animatorController;
 
         public Dice BattleDice => _battleDice;
+
+        public int MaxHealth => _maxHealth;
     }
 }
